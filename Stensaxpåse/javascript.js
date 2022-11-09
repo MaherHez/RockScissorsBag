65a31513d0e0b59b1613acfeb37e9248bbf110b8
MaherHez <96958005+MaherHez@users.noreply.github.com>
2022-11-09 21:51:11 +0100

    Add files via upload
    4

--- Stensaxpåse/javascript.js
+++ Stensaxpåse/javascript.js
@@ -1,76 +1,50 @@
-
-
-/* function selectRock() {
-    const rockSelected = document.getElementById('select-rock');
-    console.log("You selected rock!");
-    rockSelected.style.display = 'none';
-    const displayRock = document.getElementById('show-rock')
-    displayRock.style.display = 'block';
-} */
-
-/* function selectPaper() {
-    const paperSelected = document.getElementById('select-paper');
-    console.log("You selected paper!");
-    paperSelected.style.display = 'none';
-    const displaypaper = document.getElementById('show-paper')
-    displaypaper.style.display = 'block';
-} */
-
-/* function selectScissors() {
-    const scissorsSelected = document.getElementById('select-scissors');
-    console.log("You selected scissors!");
-    scissorsSelected.style.display = 'none';
-    const displayscissors = document.getElementById('show-scissors')
-    displayscissors.style.display = 'block';
-
-} */
-
-
 const a = document.getElementById('show-rock-comp');
 const b = document.getElementById('show-paper-comp');
 const c = document.getElementById('show-scissors-comp');
 
+const displaypaper = document.getElementById('show-paper');
+const displayscissors = document.getElementById('show-scissors');
+const displayRock = document.getElementById('show-rock');
+
+const rockSelected = document.getElementById('select-rock');
+const paperSelected = document.getElementById('select-paper');
+const scissorsSelected = document.getElementById('select-scissors');
+
 
 function selectRock() {
 
-    const rockSelected = document.getElementById('select-rock');
+
     console.log("You selected rock!");
     rockSelected.style.display = 'none';
-    const displayRock = document.getElementById('show-rock')
     displayRock.style.display = 'block';
 
     let computerOptions = [a, b, c];
     const computersChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
-    console.log(computersChoice)
 
     computersChoice.style.display = "block";
 
     if (computersChoice === a && displayRock === document.getElementById('show-rock')) {
         console.log("Its a tie");
 
-
-        
     }
-
     else if (computersChoice === c && displayRock === document.getElementById('show-rock')) {
-
-        console.log("You won a point!")
+        yourScore.innerHTML = Number(yourScore.innerHTML) + 1;
+        console.log(yourScore.innerHTML)
     }
 
     else if ((computersChoice === b && displayRock === document.getElementById('show-rock'))) {
-        console.log("You lost a point")
-    }
+        compScore.innerHTML = Number(compScore.innerHTML) + 1;
+        console.log(compScore.innerHTML)
 
+    }
 
 }
 
 function selectScissors() {
 
-    const scissorsSelected = document.getElementById('select-scissors');
     console.log("You selected scissors!");
     scissorsSelected.style.display = 'none';
-    const displayscissors = document.getElementById('show-scissors')
     displayscissors.style.display = 'block';
 
     let computerOptions = [a, b, c];
@@ -79,6 +53,7 @@ function selectScissors() {
     console.log(computersChoice)
 
     computersChoice.style.display = "block";
+
 
     if (computersChoice === c && displayscissors === document.getElementById('show-scissors')) {
         console.log("Its a tie");
@@ -87,20 +62,23 @@ function selectScissors() {
     else if (computersChoice === b && displayscissors === document.getElementById('show-scissors')) {
 
         console.log("You won a point!")
+        yourScore.innerHTML = Number(yourScore.innerHTML) + 1;
+        console.log(yourScore.innerHTML)
     }
 
     else if ((computersChoice === a && displayscissors === document.getElementById('show-scissors'))) {
         console.log("You lost a point")
+        compScore.innerHTML = Number(compScore.innerHTML) + 1;
+        console.log(compScore.innerHTML)
+
     }
 }
 
 
 function selectPaper() {
 
-    const paperSelected = document.getElementById('select-paper');
     console.log("You selected paper!");
     paperSelected.style.display = 'none';
-    const displaypaper = document.getElementById('show-paper')
     displaypaper.style.display = 'block';
 
 
@@ -117,17 +95,47 @@ function selectPaper() {
     }
 
     else if (computersChoice === c && displaypaper === document.getElementById('show-paper')) {
+        console.log("You lost a point!")
+        compScore.innerHTML = Number(compScore.innerHTML) + 1;
+        console.log(compScore.innerHTML)
 
-        console.log("You won a point!")
     }
 
     else if ((computersChoice === a && displaypaper === document.getElementById('show-paper'))) {
-        console.log("You lost a point")
+        console.log("You won a point!")
+        yourScore.innerHTML = Number(yourScore.innerHTML) + 1;
+        console.log(yourScore.innerHTML)
+    }
+
+}
+
+function reset() {
+
+    rockSelected.style.display = 'block';
+    displayRock.style.display = 'none';
+
+    scissorsSelected.style.display = 'block';
+    displayscissors.style.display = 'none';
+
+    paperSelected.style.display = 'block';
+    displaypaper.style.display = 'none';
+
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+
+    if (yourScore.innerHTML == 12) {
+        alert("Congratulations you won!")
+
+    }
+
+    if (compScore.innerHTML == 12) {
+        alert("Game over, you lost!")
     }
 
 
-
 }
+
 
 
 
